7ddea030e3fe3824f9a866c191f124e04bb25aa3
shadowmoses26 <dracula094@mail.ru>
2024-03-22 17:10:24 +0300

    Display validations errors

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,12 +1,14 @@
 class ContactsController < ApplicationController
 
   def new
+    #@contact = Contact.new
   end
 
   def create
     @contact = Contact.new(contact_params)
     if @contact.valid?
       @contact.save
+
     else
       render action: 'new'
     end
