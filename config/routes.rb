0c6717c58cabada24ea0c93df4190b5b4ff293a6
shadowmoses26 <dracula094@mail.ru>
2024-03-22 16:48:33 +0300

    Change /contacts/new->contacts

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resource :contacts, only: [:new,:create]
+
+  resource :contacts, only: [:new,:create], path_names: {:new => ''}
   resources :articles
 
   # Defines the root path route ("/")
