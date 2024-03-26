1315fcec44e3862d3908efaa7a64323f4ace282b
shadowmoses26 <dracula094@mail.ru>
2024-03-26 15:08:57 +0300

    Implement articles#delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,12 +3,13 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
   def show
     @article = Article.find(params[:id])
   end
+
   def new
   end
-
 
   def create
     @article = Article.new(article_params)
@@ -34,6 +35,12 @@ class ArticlesController < ApplicationController
     else
       render action: 'edit'
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
   private
