e8b96a11bc6d156acad4f1522fcab703102db9b5
shadowmoses26 <dracula094@mail.ru>
2024-04-21 12:27:04 +0300

    Permit username for sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.for(:sign_in) << :username
+    devise_parameter_sanitizer.for(:account_update) << :username
+
+
+  end
 end
