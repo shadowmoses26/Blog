1584cd2541c3d5b4b2d2bfa9d6878bef105db952
shadowmoses26 <dracula094@mail.ru>
2024-04-21 12:34:20 +0300

    Add devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,10 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :username
-    devise_parameter_sanitizer.for(:sign_in) << :username
-    devise_parameter_sanitizer.for(:account_update) << :username
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    # devise_parameter_sanitizer.for(:sign_up) << :username
+    # devise_parameter_sanitizer.for(:sign_in) << :username
+    # devise_parameter_sanitizer.for(:account_update) << :username
 
 
   end
